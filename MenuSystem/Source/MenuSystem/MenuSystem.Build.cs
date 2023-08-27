db7db3dcc198759c70e3aa3421a17d9a04b0da9e
Peter Donald <peter@realityforge.org>
2023-08-27 17:11:47 +1000

    Enable Steam OnlineSubsystem

--- MenuSystem/Source/MenuSystem/MenuSystem.Build.cs
+++ MenuSystem/Source/MenuSystem/MenuSystem.Build.cs
@@ -8,6 +8,16 @@ public class MenuSystem : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"HeadMountedDisplay",
+			"EnhancedInput",
+			"OnlineSubsystem",
+			"OnlineSubsystemSteam"
+		});
 	}
 }
