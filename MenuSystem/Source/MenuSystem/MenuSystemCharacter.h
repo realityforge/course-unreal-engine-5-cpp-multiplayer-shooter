bb3ddcf091b48d741e273dc42fbcbab32ce1316d
Peter Donald <peter@realityforge.org>
2023-08-28 17:21:16 +1000

    Whitespace

--- MenuSystem/Source/MenuSystem/MenuSystemCharacter.h
+++ MenuSystem/Source/MenuSystem/MenuSystemCharacter.h
@@ -21,7 +21,7 @@ class AMenuSystemCharacter : public ACharacter
 	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
-	
+
 	/** MappingContext */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	class UInputMappingContext* DefaultMappingContext;
@@ -40,21 +40,18 @@ class AMenuSystemCharacter : public ACharacter
 
 public:
 	AMenuSystemCharacter();
-	
 
 protected:
-
 	/** Called for movement input */
 	void Move(const FInputActionValue& Value);
 
 	/** Called for looking input */
 	void Look(const FInputActionValue& Value);
-			
 
 protected:
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
+
 	// To add mapping context
 	virtual void BeginPlay();
 
