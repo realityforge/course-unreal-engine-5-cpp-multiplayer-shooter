cd88c0da73d79e0ffbfc89f1f69cf3dd145385d4
Peter Donald <peter@realityforge.org>
2023-09-16 22:07:22 +1000

    Add initial AnimInstance stub

---
+++ Blaster/Source/Blaster/Private/Character/BlasterAnimInstance.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Animation/AnimInstance.h"
+#include "CoreMinimal.h"
+#include "BlasterAnimInstance.generated.h"
+
+UCLASS()
+class UBlasterAnimInstance : public UAnimInstance
+{
+    GENERATED_BODY()
+};
