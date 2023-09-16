f111e39afcbb5bfe36ea6e2d1e17a3dd1eb43ffb
Peter Donald <peter@realityforge.org>
2023-09-16 16:23:37 +1000

    Add a dependency on EnhancedInput

--- Blaster/Source/Blaster/Blaster.Build.cs
+++ Blaster/Source/Blaster/Blaster.Build.cs
@@ -10,7 +10,7 @@ public class Blaster : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new[] { "EnhancedInput" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
