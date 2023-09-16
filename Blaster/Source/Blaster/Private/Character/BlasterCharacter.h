0d7df8182bff68542adef4aa32c7c859ef197763
Peter Donald <peter@realityforge.org>
2023-09-16 15:32:47 +1000

    Remove some junk comments

--- Blaster/Source/Blaster/Private/Character/BlasterCharacter.h
+++ Blaster/Source/Blaster/Private/Character/BlasterCharacter.h
@@ -12,17 +12,13 @@ class ABlasterCharacter : public ACharacter
     GENERATED_BODY()
 
 public:
-    // Sets default values for this character's properties
     ABlasterCharacter();
 
 protected:
-    // Called when the game starts or when spawned
     virtual void BeginPlay() override;
 
 public:
-    // Called every frame
     virtual void Tick(float DeltaTime) override;
 
-    // Called to bind functionality to input
     virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 };
