e666353f046a8823749290c685b20ebfb8b50edb
Peter Donald <peter@realityforge.org>
2023-10-10 22:51:28 +1100

    Lesson 46 - Add a pickup widget that shows key required to pickup

--- Blaster/Source/Blaster/Private/Weapon/Weapon.cpp
+++ Blaster/Source/Blaster/Private/Weapon/Weapon.cpp
@@ -1,5 +1,7 @@
 #include "Weapon/Weapon.h"
+#include "Character/BlasterCharacter.h"
 #include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
 
 AWeapon::AWeapon()
 {
@@ -19,6 +21,9 @@ AWeapon::AWeapon()
     AreaSphere->SetupAttachment(WeaponMesh);
     AreaSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
     AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+    PickupWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickupWidget"));
+    PickupWidget->SetupAttachment(WeaponMesh);
 }
 
 void AWeapon::BeginPlay()
@@ -30,6 +35,27 @@ void AWeapon::BeginPlay()
         // We only enable the collision on the server
         AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
         AreaSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+        AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnAreaSphereOverlap);
+    }
+    if (PickupWidget)
+    {
+        PickupWidget->SetVisibility(false);
+    }
+}
+
+void AWeapon::OnAreaSphereOverlap(UPrimitiveComponent* OverlappedComponent,
+                                  AActor* OtherActor,
+                                  UPrimitiveComponent* OtherComp,
+                                  int32 OtherBodyIndex,
+                                  bool bFromSweep,
+                                  const FHitResult& SweepResult)
+{
+    if ([[maybe_unused]] const auto BlasterCharacter = Cast<ABlasterCharacter>(OtherActor))
+    {
+        if (PickupWidget)
+        {
+            PickupWidget->SetVisibility(true);
+        }
     }
 }
 
--- Blaster/Source/Blaster/Private/Weapon/Weapon.h
+++ Blaster/Source/Blaster/Private/Weapon/Weapon.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "Weapon.generated.h"
 
+class UWidgetComponent;
 class USphereComponent;
 
 UENUM(BlueprintType)
@@ -31,6 +32,14 @@ public:
 protected:
     virtual void BeginPlay() override;
 
+    UFUNCTION()
+    virtual void OnAreaSphereOverlap(UPrimitiveComponent* OverlappedComponent,
+                                     AActor* OtherActor,
+                                     UPrimitiveComponent* OtherComp,
+                                     int32 OtherBodyIndex,
+                                     bool bFromSweep,
+                                     const FHitResult& SweepResult);
+
 private:
     UPROPERTY(VisibleAnywhere, Category = "Weapon Properties", meta = (AllowPrivateAccess))
     TObjectPtr<USkeletalMeshComponent> WeaponMesh;
@@ -40,4 +49,7 @@ private:
 
     UPROPERTY(VisibleAnywhere, Category = "Weapon Properties", meta = (AllowPrivateAccess))
     EWeaponState WeaponState;
+
+    UPROPERTY(VisibleAnywhere, Category = "Weapon Properties", meta = (AllowPrivateAccess))
+    TObjectPtr<UWidgetComponent> PickupWidget;
 };
