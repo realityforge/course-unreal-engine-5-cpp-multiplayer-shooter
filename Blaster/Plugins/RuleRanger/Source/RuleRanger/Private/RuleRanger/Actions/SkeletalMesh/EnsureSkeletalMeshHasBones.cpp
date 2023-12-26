fc74e873449b5f6ed518e1e0202219eef4536ac5
Peter Donald <peter@realityforge.org>
2023-12-26 22:18:01 +1100

    Convert specific RuleRanger action to generic action and migrate action to RuleRanger

---
+++ Blaster/Plugins/RuleRanger/Source/RuleRanger/Private/RuleRanger/Actions/SkeletalMesh/EnsureSkeletalMeshHasBones.cpp
@@ -0,0 +1,47 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "EnsureSkeletalMeshHasBones.h"
+
+void UEnsureSkeletalMeshHasBones::Apply_Implementation(URuleRangerActionContext* ActionContext, UObject* Object)
+{
+    if (IsValid(Object))
+    {
+        if (const auto SkeletalMesh = Cast<USkeletalMesh>(Object))
+        {
+            if (ensure(SkeletalMesh->GetSkeleton()))
+            {
+                const USkeleton* Skeleton = SkeletalMesh->GetSkeleton();
+                for (const auto BoneName : Bones)
+                {
+                    if (INDEX_NONE == Skeleton->GetReferenceSkeleton().FindBoneIndex(BoneName))
+                    {
+                        const auto OutMessage = FString::Printf(TEXT("Bone named '%s' does not exist on "
+                                                                     "the SkeletalMesh named '%s' with Skeleton named  "
+                                                                     "%s component. Reason: %s"),
+                                                                *BoneName.ToString(),
+                                                                *SkeletalMesh->GetFullName(),
+                                                                *Skeleton->GetFullName(),
+                                                                *Reason);
+                        ActionContext->Error(FText::FromString(OutMessage));
+                    }
+                }
+            }
+        }
+        else
+        {
+            LogError(Object, TEXT("Action running on class that is not a SkeletalMesh"));
+        }
+    }
+}
--- Blaster/Plugins/RuleRanger/Source/RuleRanger/Private/RuleRanger/Actions/SkeletalMesh/EnsureSkeletalMeshHasBones.h
+++ Blaster/Plugins/RuleRanger/Source/RuleRanger/Private/RuleRanger/Actions/SkeletalMesh/EnsureSkeletalMeshHasBones.h
@@ -17,17 +17,27 @@
 #include "CoreMinimal.h"
 #include "Engine/DataTable.h"
 #include "RuleRangerAction.h"
-#include "EnsureSkeletalMeshHasRequiredBonesForFabrik.generated.h"
+#include "EnsureSkeletalMeshHasBones.generated.h"
 
+/**
+ * Action to check that the SkeletalMesh assets contain the specified bones.
+ */
 UCLASS(AutoExpandCategories = ("Rule Ranger"),
        Blueprintable,
        BlueprintType,
        CollapseCategories,
        DefaultToInstanced,
        EditInlineNew)
-class UEnsureSkeletalMeshHasRequiredBonesForFabrik final : public URuleRangerAction
+class RULERANGER_API UEnsureSkeletalMeshHasBones final : public URuleRangerAction
 {
     GENERATED_BODY()
+
+    /** The names of the bones that must be present on the SkeletalMesh. */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rule Ranger", meta = (AllowPrivateAccess))
+    TSet<FName> Bones;
+    /** The reason why we must have the bones. Used when emitting an error. */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rule Ranger", meta = (AllowPrivateAccess))
+    FString Reason{ "" };
 
 public:
     virtual void Apply_Implementation(URuleRangerActionContext* ActionContext, UObject* Object) override;
