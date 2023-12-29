409e5457ae4b3b04f27b8ba1461f12469f893cdc
Peter Donald <peter@realityforge.org>
2023-12-29 19:33:32 +1100

    Destroy the casing and play a sound when it hits a surface

--- Blaster/Source/Blaster/Private/Weapon/Casing.cpp
+++ Blaster/Source/Blaster/Private/Weapon/Casing.cpp
@@ -1,15 +1,41 @@
 #include "Weapon/Casing.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 
 ACasing::ACasing()
 {
     PrimaryActorTick.bCanEverTick = false;
     CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
+    // Stop a camera jumping in (due to occlusion) when the a casing flies between the camera and the character
+    CasingMesh->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+
+    CasingMesh->SetSimulatePhysics(true);
+    CasingMesh->SetEnableGravity(true);
+
+    // Make sure we get OnHit called
+    CasingMesh->SetNotifyRigidBodyCollision(true);
+
     SetRootComponent(CasingMesh);
 }
 
 void ACasing::BeginPlay()
 {
     Super::BeginPlay();
+    CasingMesh->OnComponentHit.AddDynamic(this, &ACasing::OnHit);
+    CasingMesh->AddImpulse(GetActorForwardVector() * InitialImpulse);
+}
+
+void ACasing::OnHit(UPrimitiveComponent* HitComponent,
+                    AActor* OtherActor,
+                    UPrimitiveComponent* OtherComponent,
+                    FVector NormalImpulse,
+                    const FHitResult& Hit)
+{
+    if (ShellSound)
+    {
+        UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+    }
+    Destroy();
 }
 
 void ACasing::Tick(float DeltaTime)
--- Blaster/Source/Blaster/Public/Weapon/Casing.h
+++ Blaster/Source/Blaster/Public/Weapon/Casing.h
@@ -4,6 +4,8 @@
 #include "GameFramework/Actor.h"
 #include "Casing.generated.h"
 
+class USoundCue;
+
 UCLASS(Abstract)
 class BLASTER_API ACasing : public AActor
 {
@@ -12,8 +14,21 @@ class BLASTER_API ACasing : public AActor
     UPROPERTY(VisibleAnywhere)
     UStaticMeshComponent* CasingMesh{ nullptr };
 
+    UPROPERTY(EditDefaultsOnly)
+    USoundCue* ShellSound{ nullptr };
+
+    UPROPERTY(EditDefaultsOnly)
+    float InitialImpulse{ 10.f };
+
 protected:
     virtual void BeginPlay() override;
+
+    UFUNCTION()
+    virtual void OnHit(UPrimitiveComponent* HitComponent,
+                       AActor* OtherActor,
+                       UPrimitiveComponent* OtherComponent,
+                       FVector NormalImpulse,
+                       const FHitResult& Hit);
 
 public:
     ACasing();
