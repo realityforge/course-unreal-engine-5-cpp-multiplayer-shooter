77b45b2079e88badb642b838a4e03553ecc27caa
Peter Donald <peter@realityforge.org>
2024-01-06 13:40:06 +1100

    Add a hit react montarget and derive addirive animation sequences for them

--- Blaster/Source/Blaster/Blaster.h
+++ Blaster/Source/Blaster/Blaster.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+// Create a custom collision Channel so can specifically collide with mesh of characters for more accurate hits
+// (If we use ECC_Pawn then we would also collide with Capsule of Character which is less precise)
+#define ECC_SkeletalMesh ECollisionChannel::ECC_GameTraceChannel1
