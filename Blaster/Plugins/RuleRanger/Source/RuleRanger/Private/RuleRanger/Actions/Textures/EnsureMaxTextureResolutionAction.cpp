47c99dbba215b96ee516c48e7a1f46b782b10932
Peter Donald <peter@realityforge.org>
2024-01-07 22:40:57 +1100

    Add an action and a Rule to ensure textures do not exceed max dimensions. Consolidate the Texture related rules into a single RuleSet

---
+++ Blaster/Plugins/RuleRanger/Source/RuleRanger/Private/RuleRanger/Actions/Textures/EnsureMaxTextureResolutionAction.cpp
@@ -0,0 +1,45 @@
+﻿/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "EnsureMaxTextureResolutionAction.h"
+
+void UEnsureMaxTextureResolutionAction::Apply_Implementation(URuleRangerActionContext* ActionContext, UObject* Object)
+{
+    if (IsValid(Object))
+    {
+        if (const auto Texture = Cast<UTexture2D>(Object); !Texture)
+        {
+            LogError(Object, TEXT("Attempt to run on Object that is not a Texture2D instance."));
+        }
+        else
+        {
+            const int32 TexSizeX = Texture->GetSizeX();
+            const int32 TexSizeY = Texture->GetSizeY();
+
+            if (TexSizeX > MaxSizeX || TexSizeY > MaxSizeY)
+            {
+                FFormatNamedArguments Arguments;
+                Arguments.Add(TEXT("TexSizeX"), FText::FromString(FString::FromInt(TexSizeX)));
+                Arguments.Add(TEXT("TexSizeY"), FText::FromString(FString::FromInt(TexSizeY)));
+                Arguments.Add(TEXT("MaxSizeX"), FText::FromString(FString::FromInt(MaxSizeX)));
+                Arguments.Add(TEXT("MaxSizeY"), FText::FromString(FString::FromInt(MaxSizeY)));
+
+                ActionContext->Error(FText::Format(NSLOCTEXT("RuleRanger",
+                                                             "EnsureMaxTextureResolutionAction_Error",
+                                                             "Texture dimensions {TexSizeX}x{TexSizeY} exceed the maximum dimensions {MaxSizeX}x{MaxSizeY}."),
+                                                   Arguments));
+            }
+        }
+    }
+}
---
+++ Blaster/Plugins/RuleRanger/Source/RuleRanger/Private/RuleRanger/Actions/Textures/EnsureMaxTextureResolutionAction.h
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "RuleRangerAction.h"
+#include "EnsureMaxTextureResolutionAction.generated.h"
+
+/**
+ * Action to check that a Texture dimensions comply with the specified constraint.
+ */
+UCLASS(AutoExpandCategories = ("Rule Ranger"),
+       Blueprintable,
+       BlueprintType,
+       CollapseCategories,
+       DefaultToInstanced,
+       EditInlineNew)
+class RULERANGER_API UEnsureMaxTextureResolutionAction final : public URuleRangerAction
+{
+    GENERATED_BODY()
+
+    /**
+     * The maximum resolution of the texture in X dimension.
+     * If set to 0 then there is no maximum resolution.
+     */
+    UPROPERTY(EditAnywhere)
+    int32 MaxSizeX{ 4096 };
+
+    /**
+     * The maximum resolution of the texture in Y dimension.
+     * If set to 0 then there is no maximum resolution.
+     */
+    UPROPERTY(EditAnywhere)
+    int32 MaxSizeY{ 4096 };
+
+public:
+    virtual void Apply_Implementation(URuleRangerActionContext* ActionContext, UObject* Object) override;
+};
