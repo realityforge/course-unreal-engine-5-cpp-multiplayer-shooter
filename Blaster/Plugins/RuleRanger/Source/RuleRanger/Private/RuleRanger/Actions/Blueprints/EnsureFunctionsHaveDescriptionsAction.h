371565930f264968268b5a4e8ab7d4248870084c
Peter Donald <peter@realityforge.org>
2024-01-08 23:11:16 +1100

    Add rule that ensures blueprints functions are documented if they are public or optionally protected

---
+++ Blaster/Plugins/RuleRanger/Source/RuleRanger/Private/RuleRanger/Actions/Blueprints/EnsureFunctionsHaveDescriptionsAction.h
@@ -0,0 +1,40 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RuleRangerAction.h"
+#include "EnsureFunctionsHaveDescriptionsAction.generated.h"
+
+/**
+ * Action to check that the functions defined in the Blueprint have descriptions.
+ */
+UCLASS(AutoExpandCategories = ("Rule Ranger"),
+       Blueprintable,
+       BlueprintType,
+       CollapseCategories,
+       DefaultToInstanced,
+       EditInlineNew)
+class RULERANGER_API UEnsureFunctionsHaveDescriptionsAction final : public URuleRangerAction
+{
+    GENERATED_BODY()
+
+    /** Should the action check protected functions. */
+    UPROPERTY(EditAnywhere, Category = "Rule Ranger")
+    bool bCheckProtectedFunctions{ true };
+
+public:
+    virtual void Apply_Implementation(URuleRangerActionContext* ActionContext, UObject* Object) override;
+};
