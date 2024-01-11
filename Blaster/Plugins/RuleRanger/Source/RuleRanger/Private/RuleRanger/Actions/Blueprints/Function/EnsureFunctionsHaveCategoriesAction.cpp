477656f5bcf82c527f6f345170f5883102202ad3
Peter Donald <peter@realityforge.org>
2024-01-12 10:25:21 +1100

    Add action to ensure that Blueprint functions are categorized if they exceed a threshold

---
+++ Blaster/Plugins/RuleRanger/Source/RuleRanger/Private/RuleRanger/Actions/Blueprints/Function/EnsureFunctionsHaveCategoriesAction.cpp
@@ -0,0 +1,49 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "EnsureFunctionsHaveCategoriesAction.h"
+
+bool UEnsureFunctionsHaveCategoriesAction::ShouldAnalyzeBlueprint(UBlueprint* Blueprint) const
+{
+    // -1 to skip the construction graph
+    const int32 FunctionCount = Blueprint->FunctionGraphs.Num() - 1;
+    const bool bAnalyze = FunctionCount >= Threshold;
+    if (!bAnalyze)
+    {
+        LogInfo(Blueprint,
+                FString::Printf(TEXT("The number of functions in the Blueprint (%d)"
+                                     " is below the threshold (%d) so it is not "
+                                     "necessary to enforce categorisation of the functions."),
+                                FunctionCount,
+                                Threshold));
+    }
+    return bAnalyze;
+}
+
+void UEnsureFunctionsHaveCategoriesAction::AnalyzeFunction(URuleRangerActionContext* ActionContext,
+                                                           UBlueprint* Blueprint,
+                                                           UK2Node_FunctionEntry* FunctionEntry,
+                                                           UEdGraph* Graph)
+{
+    if (FunctionEntry->MetaData.Category.ToString().Equals(TEXT("Default")))
+    {
+        const auto& ErrorMessage =
+            FString::Printf(TEXT("Blueprint contains a function named '%s' that is in the Default "
+                                 "category but it is expected to have a non-default category specified "
+                                 "as the Blueprint has more than %d functions."),
+                            *Graph->GetName(),
+                            Threshold);
+        ActionContext->Error(FText::FromString(ErrorMessage));
+    }
+}
---
+++ Blaster/Plugins/RuleRanger/Source/RuleRanger/Private/RuleRanger/Actions/Blueprints/Function/EnsureFunctionsHaveCategoriesAction.h
@@ -0,0 +1,43 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "BaseAnalyzeFunctionAction.h"
+#include "CoreMinimal.h"
+#include "K2Node_FunctionEntry.h"
+#include "RuleRangerAction.h"
+#include "EnsureFunctionsHaveCategoriesAction.generated.h"
+
+/**
+ * Action to check that the functions defined in the Blueprint have a category if the Blueprint crosses a threshold
+ * number of functions.
+ */
+UCLASS(AutoExpandCategories = ("Rule Ranger"))
+class RULERANGER_API UEnsureFunctionsHaveCategoriesAction final : public UBaseAnalyzeFunctionAction
+{
+    GENERATED_BODY()
+
+    /** The number of functions that triggers requirement that all functions have a category. */
+    UPROPERTY(EditAnywhere, Category = "Rule Ranger")
+    int32 Threshold{ 5 };
+
+protected:
+    virtual bool ShouldAnalyzeBlueprint(UBlueprint* Blueprint) const override;
+
+    virtual void AnalyzeFunction(URuleRangerActionContext* ActionContext,
+                                 UBlueprint* Blueprint,
+                                 UK2Node_FunctionEntry* FunctionEntry,
+                                 UEdGraph* Graph) override;
+};
