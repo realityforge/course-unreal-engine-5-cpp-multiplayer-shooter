2e567b61e5b1707be3c135c020e497a1ba43782a
Peter Donald <peter@realityforge.org>
2024-01-14 14:51:45 +1100

    Add rule to verify the sample rate of audio

---
+++ Blaster/Plugins/RuleRanger/Source/RuleRanger/Private/RuleRanger/Actions/SoundWave/EnsureSoundWaveSampleRateValidAction.cpp
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "EnsureSoundWaveSampleRateValidAction.h"
+
+UEnsureSoundWaveSampleRateValidAction::UEnsureSoundWaveSampleRateValidAction()
+{
+    ValidSampleRates.Add(48000.0f);
+}
+
+void UEnsureSoundWaveSampleRateValidAction::Apply_Implementation(URuleRangerActionContext* ActionContext,
+                                                                 UObject* Object)
+{
+    const USoundWave* SoundWave = CastChecked<USoundWave>(Object);
+
+    // ReSharper disable once CppTooWideScopeInitStatement
+    const float SampleRate = SoundWave->GetSampleRateForCurrentPlatform();
+    if (!ValidSampleRates.Contains(SampleRate))
+    {
+        FString ValidValues{ "" };
+        for (const float ValidSampleRate : ValidSampleRates)
+        {
+            if (0 != ValidValues.Len())
+            {
+                ValidValues.Append(", ");
+            }
+            ValidValues.Appendf(TEXT("%f"), ValidSampleRate);
+        }
+
+        const auto& ErrorMessage = FString::Printf(TEXT("SoundWave has a sample rate of %f but must have one of: %s"),
+                                                   SampleRate,
+                                                   *FString::Join(ValidValues, TEXT(", ")));
+        ActionContext->Error(FText::FromString(ErrorMessage));
+    }
+}
+
+UClass* UEnsureSoundWaveSampleRateValidAction::GetExpectedType()
+{
+    return USoundWave::StaticClass();
+}
