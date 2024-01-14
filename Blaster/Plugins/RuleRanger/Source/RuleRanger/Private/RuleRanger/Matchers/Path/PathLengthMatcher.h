6ee50ceca56b62aafaaecda6912e4b4fd5de6015
Peter Donald <peter@realityforge.org>
2024-01-14 19:53:10 +1100

    Add rule to ensure that the path for any asset is not too long

---
+++ Blaster/Plugins/RuleRanger/Source/RuleRanger/Private/RuleRanger/Matchers/Path/PathLengthMatcher.h
@@ -0,0 +1,35 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RuleRangerMatcher.h"
+#include "UObject/Object.h"
+#include "PathLengthMatcher.generated.h"
+
+/**
+ * Matcher that returns true if object has path that is above maximum length.
+ */
+UCLASS()
+class RULERANGER_API UPathLengthMatcher final : public URuleRangerMatcher
+{
+    GENERATED_BODY()
+
+    /** The length after which the matcher will match. */
+    UPROPERTY(EditAnywhere)
+    int32 MaxPathLength{ 140 };
+
+public:
+    virtual bool Test_Implementation(UObject* Object) override;
+};
