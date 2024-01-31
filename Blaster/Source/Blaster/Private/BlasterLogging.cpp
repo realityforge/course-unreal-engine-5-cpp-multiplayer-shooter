dc0f2da3d7cb5ed09283d79c7d06e44a2bdd8685
Peter Donald <peter@realityforge.org>
2024-01-31 16:17:36 +1100

    Add some debugging support

---
+++ Blaster/Source/Blaster/Private/BlasterLogging.cpp
@@ -0,0 +1,57 @@
+﻿#include "BlasterLogging.h"
+
+// The implementation of log category top use within application
+DEFINE_LOG_CATEGORY(Blaster);
+
+FString BlasterGetClientServerContextString(const UObject* Object)
+{
+    if (!Object)
+    {
+        return TEXT("?");
+    }
+    else
+    {
+        FString Context{ TEXT("") };
+        const auto World = Object->GetWorld();
+        if (EWorldType::Game == World->WorldType)
+        {
+            Context.Append("Game");
+        }
+        else if (EWorldType::PIE == World->WorldType)
+        {
+            Context.Append("PIE");
+        }
+        else if (EWorldType::Editor == World->WorldType)
+        {
+            Context.Append("Editor");
+        }
+        else if (EWorldType::EditorPreview == World->WorldType)
+        {
+            Context.Append("EditorPreview");
+        }
+        else if (EWorldType::GamePreview == World->WorldType)
+        {
+            Context.Append("GamePreview");
+        }
+        else if (EWorldType::GameRPC == World->WorldType)
+        {
+            Context.Append("GameRPC");
+        }
+        else if (World->IsPlayInPreview())
+        {
+            Context.Append("Preview");
+        }
+        // ReSharper disable once CppTooWideScopeInitStatement
+        const ENetMode NetMode = World->GetNetMode();
+        if (NM_Client == NetMode)
+        {
+            Context.Append(":Client");
+        }
+        else if (NM_DedicatedServer == NetMode || NM_ListenServer == NetMode)
+        {
+            Context.Append(":Server");
+        }
+
+        return Context;
+    }
+}
---
+++ Blaster/Source/Blaster/Private/BlasterLogging.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "Logging/LogCategory.h"
+
+// Log category to use within the application
+DECLARE_LOG_CATEGORY_EXTERN(Blaster, Verbose, All);
+
+FString BlasterGetClientServerContextString(const UObject* Object);
+
+// ReSharper disable once CppUE4CodingStandardNamingViolationWarning
+#define _BL_ULOG(LogLevel, fmt, ...)                   \
+    UE_LOG(Blaster,                                    \
+           LogLevel,                                   \
+           TEXT("[%s] %s:%i [%s]->[%s] %s"),           \
+           *BlasterGetClientServerContextString(this), \
+           *FString(__FUNCTION__),                     \
+           __LINE__,                                   \
+           *GetNameSafe(GetOwner()),                   \
+           *GetNameSafe(this),                         \
+           *FString::Printf(TEXT(fmt), ##__VA_ARGS__))
+
+/**
+ * BL_LOG*
+ *
+ *   You can use these macro in any code where `this` evaluates to a valid UObject.
+ *   The log will print with the __FUNCTION__ and __LINE__ where you write the macro,
+ *   and will report `this` identity as [OwnerName]->[ThisName] in the log message.
+ *   The message that you pass in (fmt, ...) is appended to the rest of the log info.
+ */
+#define BL_ULOG(fmt, ...) _BL_ULOG(Log, fmt, ##__VA_ARGS__)
+#define BL_ULOG_WARNING(fmt, ...) _BL_ULOG(Warning, fmt, ##__VA_ARGS__)
+#define BL_ULOG_ERROR(fmt, ...) _BL_ULOG(Error, fmt, ##__VA_ARGS__)
+
+// TODO: We should make these macros be no-ops when releasing packaged product?
+
+#define BL_ULOG_VERBOSE(fmt, ...) _BL_ULOG(Verbose, fmt, ##__VA_ARGS__)
+#define BL_ULOG_VERY_VERBOSE(fmt, ...) _BL_ULOG(VeryVerbose, fmt, ##__VA_ARGS__)
